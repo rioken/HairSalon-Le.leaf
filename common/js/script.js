920742b6db96d9cbb3407add4f50eb9f95caef51
rioken <77283499+rioken@users.noreply.github.com>
2025-03-19 13:55:49 +0900

    Add files via upload

---
+++ common/js/script.js
@@ -0,0 +1,50 @@
+
+//topbtn//
+$(function () {
+    var pagetop = $('#page-top');
+    pagetop.hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1000) {
+            pagetop.fadeIn();
+        } else {
+            pagetop.fadeOut();
+        }
+    });
+    pagetop.click(function () {
+        $('body, html').animate({ scrollTop: 0 }, 500);
+        return false;
+    });
+});
+
+//スマホハンバーガーメニュー//
+(function ($) {
+    $(function () {
+        $('#nav-toggle').on('click', function () {
+            $('body').toggleClass('open');
+        });
+    });
+})(jQuery);
+$(function () {
+    $('#nav-toggle').on('click', function () {
+        $('#gNavi, #shoptime').toggleClass('is-active');
+    });
+}());
+
+
+//skideshow//
+const smoothScrollTrigger = document.querySelectorAll('a[href^="#"]');
+for (let i = 0; i < smoothScrollTrigger.length; i++) {
+    smoothScrollTrigger[i].addEventListener('click', (e) => {
+        e.preventDefault();
+        let href = smoothScrollTrigger[i].getAttribute('href');
+        let targetElement = document.getElementById(href.replace('#', ''));
+        const rect = targetElement.getBoundingClientRect().top;
+        const offset = window.pageYOffset;
+        const gap = 60;
+        const target = rect + offset - gap;
+        window.scrollTo({
+            top: target,
+            behavior: 'smooth',
+        });
+    });
+}
